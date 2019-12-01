0272aa5310a319ea521306b87cfd731543732fd6
Tomáš Chaloupka <chalucha@gmail.com>
2019-12-01 18:36:20 +0100

    some docs

--- source/during/io_uring.d
+++ source/during/io_uring.d
@@ -1,7 +1,9 @@
 /**
  * io_uring system api definitions.
+ *
  * See: https://github.com/torvalds/linux/blob/master/include/uapi/linux/io_uring.h
- * Last modifications from: 1d7bb1d50fb4dc141c7431cc21fdd24ffcc83c76 (20191109)
+ *
+ * Last changes from: 1d7bb1d50fb4dc141c7431cc21fdd24ffcc83c76 (20191109)
 */
 module during.io_uring;
 
@@ -13,8 +15,9 @@ import core.sys.posix.signal;
 @system nothrow @nogc:
 
 /**
- * IO submission data structure (Submission Queue Entry).
- * C: `struct io_uring_sqe`
+ * IO operation submission data structure (Submission queue entry).
+ *
+ * C API: `struct io_uring_sqe`
  */
 struct SubmissionEntry
 {
@@ -174,33 +177,33 @@ union SubmissionEntryExtraData
 enum Operation : ubyte
 {
     // available in kernel 5.1
-    NOP = 0,            /// IORING_OP_NOP
-    READV = 1,          /// IORING_OP_READV
-    WRITEV = 2,         /// IORING_OP_WRITEV
-    FSYNC = 3,          /// IORING_OP_FSYNC
-    READ_FIXED = 4,      /// IORING_OP_READ_FIXED
-    WRITE_FIXED = 5,     /// IORING_OP_WRITE_FIXED
-    POLL_ADD = 6,        /// IORING_OP_POLL_ADD
-    POLL_REMOVE = 7,     /// IORING_OP_POLL_REMOVE
+    NOP = 0,                /// IORING_OP_NOP
+    READV = 1,              /// IORING_OP_READV
+    WRITEV = 2,             /// IORING_OP_WRITEV
+    FSYNC = 3,              /// IORING_OP_FSYNC
+    READ_FIXED = 4,         /// IORING_OP_READ_FIXED
+    WRITE_FIXED = 5,        /// IORING_OP_WRITE_FIXED
+    POLL_ADD = 6,           /// IORING_OP_POLL_ADD
+    POLL_REMOVE = 7,        /// IORING_OP_POLL_REMOVE
 
     // available in kernel 5.2
-    SYNC_FILE_RANGE = 8,  /// IORING_OP_SYNC_FILE_RANGE
+    SYNC_FILE_RANGE = 8,    /// IORING_OP_SYNC_FILE_RANGE
 
     // available in kernel 5.3
-    SENDMSG = 9,        /// IORING_OP_SENDMSG
-    RECVMSG = 10,       /// IORING_OP_RECVMSG
+    SENDMSG = 9,            /// IORING_OP_SENDMSG
+    RECVMSG = 10,           /// IORING_OP_RECVMSG
 
     // available in kernel 5.4
-    TIMEOUT = 11,       /// IORING_OP_TIMEOUT
+    TIMEOUT = 11,           /// IORING_OP_TIMEOUT
 
     // probably'll be available in kernel 5.5 (in master now)
-    TIMEOUT_REMOVE = 12, /// IORING_OP_TIMEOUT_REMOVE
-    ACCEPT = 13,        /// IORING_OP_ACCEPT
-    ASYNC_CANCEL = 14,   /// IORING_OP_ASYNC_CANCEL
-    LINK_TIMEOUT = 15,   /// IORING_OP_LINK_TIMEOUT
+    TIMEOUT_REMOVE = 12,    /// IORING_OP_TIMEOUT_REMOVE
+    ACCEPT = 13,            /// IORING_OP_ACCEPT
+    ASYNC_CANCEL = 14,      /// IORING_OP_ASYNC_CANCEL
+    LINK_TIMEOUT = 15,      /// IORING_OP_LINK_TIMEOUT
 
     // not accepted yet
-    CONNECT = 16,       /// IORING_OP_CONNECT
+    CONNECT = 16,           /// IORING_OP_CONNECT
 }
 
 /// sqe->flags
@@ -214,7 +217,8 @@ enum SubmissionEntryFlags : ubyte
 
 /**
  * IO completion data structure (Completion Queue Entry)
- * C: `struct io_uring_cqe`
+ *
+ * C API: `struct io_uring_cqe`
  */
 struct CompletionEntry
 {
@@ -225,7 +229,8 @@ struct CompletionEntry
 
 /**
  * Passed in for io_uring_setup(2). Copied back with updated info on success.
- * C: `struct io_uring_params`
+ *
+ * C API: `struct io_uring_params`
  */
 struct SetupParameters
 {
@@ -266,56 +271,64 @@ enum SetupFlags : uint
     /// No flags set
     NONE    = 0,
 
-    /** `IORING_SETUP_IOPOLL`
-     *  Perform busy-waiting for an I/O completion, as opposed to getting notifications via an
-     *  asynchronous IRQ (Interrupt Request).  The file system (if any) and block device must
-     *  support polling in order for  this  to  work. Busy-waiting  provides  lower latency, but may
-     *  consume more CPU resources than interrupt driven I/O.  Currently, this feature is usable
-     *  only on a file descriptor opened using the O_DIRECT flag.  When a read or write is submitted
-     *  to a polled context, the application must poll for completions on the CQ ring by calling
-     *  io_uring_enter(2).  It is illegal to mix and match polled and non-polled I/O on an io_uring
-     *  instance.
+    /**
+     * `IORING_SETUP_IOPOLL`
+     *
+     * Perform busy-waiting for an I/O completion, as opposed to getting notifications via an
+     * asynchronous IRQ (Interrupt Request).  The file system (if any) and block device must
+     * support polling in order for  this  to  work. Busy-waiting  provides  lower latency, but may
+     * consume more CPU resources than interrupt driven I/O.  Currently, this feature is usable
+     * only on a file descriptor opened using the O_DIRECT flag.  When a read or write is submitted
+     * to a polled context, the application must poll for completions on the CQ ring by calling
+     * io_uring_enter(2).  It is illegal to mix and match polled and non-polled I/O on an io_uring
+     * instance.
      */
     IOPOLL  = 1U << 0,
 
-    /** `IORING_SETUP_SQPOLL`
-     *  When this flag is specified, a kernel thread is created to perform submission queue polling.
-     *  An io_uring instance configured in this way enables an application to issue I/O without ever
-     *  context switching into the kernel.
-     *  By using the submission queue to fill in new submission queue entries and watching for
-     *  completions on the completion queue, the application can submit and reap I/Os without doing
-     *  a single system call.
-     *  If the kernel thread is idle for more than sq_thread_idle microseconds, it will set the
-     *  IORING_SQ_NEED_WAKEUP bit in the flags field of the struct io_sq_ring. When this happens,
-     *  the application must call io_uring_enter(2) to wake the kernel thread. If I/O is kept busy,
-     *  the kernel thread will never sleep. An application making use of this feature will need to
-     *  guard the io_uring_enter(2) call with  the  following  code sequence:
+    /**
+     * `IORING_SETUP_SQPOLL`
      *
-     *      ````
-     *      // Ensure that the wakeup flag is read after the tail pointer has been written.
-     *      smp_mb();
-     *      if (*sq_ring->flags & IORING_SQ_NEED_WAKEUP)
-     *          io_uring_enter(fd, 0, 0, IORING_ENTER_SQ_WAKEUP);
-     *      ```
+     * When this flag is specified, a kernel thread is created to perform submission queue polling.
+     * An io_uring instance configured in this way enables an application to issue I/O without ever
+     * context switching into the kernel.
+     * By using the submission queue to fill in new submission queue entries and watching for
+     * completions on the completion queue, the application can submit and reap I/Os without doing
+     * a single system call.
+     * If the kernel thread is idle for more than sq_thread_idle microseconds, it will set the
+     * IORING_SQ_NEED_WAKEUP bit in the flags field of the struct io_sq_ring. When this happens,
+     * the application must call io_uring_enter(2) to wake the kernel thread. If I/O is kept busy,
+     * the kernel thread will never sleep. An application making use of this feature will need to
+     * guard the io_uring_enter(2) call with  the  following  code sequence:
      *
-     *  where sq_ring is a submission queue ring setup using the struct io_sqring_offsets described below.
+     *     ````
+     *     // Ensure that the wakeup flag is read after the tail pointer has been written.
+     *     smp_mb();
+     *     if (*sq_ring->flags & IORING_SQ_NEED_WAKEUP)
+     *         io_uring_enter(fd, 0, 0, IORING_ENTER_SQ_WAKEUP);
+     *     ```
      *
-     *  To  successfully  use this feature, the application must register a set of files to be used for
-     *  IO through io_uring_register(2) using the IORING_REGISTER_FILES opcode. Failure to do so will
-     *  result in submitted IO being errored with EBADF.
+     * where sq_ring is a submission queue ring setup using the struct io_sqring_offsets described below.
+     *
+     * To  successfully  use this feature, the application must register a set of files to be used for
+     * IO through io_uring_register(2) using the IORING_REGISTER_FILES opcode. Failure to do so will
+     * result in submitted IO being errored with EBADF.
      */
     SQPOLL  = 1U << 1,
 
-    /** `IORING_SETUP_SQ_AFF`
-     *   If this flag is specified, then the poll thread will be bound to the cpu set in the
-     *   sq_thread_cpu field of the struct io_uring_params.  This flag is only meaningful when
-     *   IORING_SETUP_SQPOLL is specified.
+    /**
+     * `IORING_SETUP_SQ_AFF`
+     *
+     *  If this flag is specified, then the poll thread will be bound to the cpu set in the
+     *  sq_thread_cpu field of the struct io_uring_params.  This flag is only meaningful when
+     *  IORING_SETUP_SQPOLL is specified.
      */
     SQ_AFF  = 1U << 2,
 
-    /** `IORING_SETUP_CQSIZE`
-     *  Create the completion queue with struct io_uring_params.cq_entries entries.  The value must
-     *  be greater than entries, and may be rounded up to the next power-of-two.
+    /**
+     * `IORING_SETUP_CQSIZE`
+     *
+     * Create the completion queue with struct io_uring_params.cq_entries entries.  The value must
+     * be greater than entries, and may be rounded up to the next power-of-two.
      */
     CQSIZE  = 1U << 3,
 }
@@ -330,7 +343,8 @@ enum SetupFeatures : uint
 
 /**
  * Filled with the offset for mmap(2)
- * C: `struct io_sqring_offsets`
+ *
+ * C API: `struct io_sqring_offsets`
  */
 struct SubmissionQueueRingOffsets
 {
@@ -375,7 +389,9 @@ enum SubmissionQueueFlags: uint
 }
 
 /**
- * C: `struct io_cqring_offsets`
+ * Field offsets used to map kernel structure to our.
+ *
+ * C API: `struct io_cqring_offsets`
  */
 struct CompletionQueueRingOffsets
 {
@@ -401,7 +417,7 @@ struct CompletionQueueRingOffsets
 	/// completion event because the ring was full.
     uint overflow;
 
-    ///
+    /// Offset to array of completion queue entries
     uint cqes;
 
     private ulong[2] resv; // reserved
@@ -410,12 +426,66 @@ struct CompletionQueueRingOffsets
 /// io_uring_register(2) opcodes and arguments
 enum RegisterOpCode : uint
 {
+    /**
+     * arg points to a struct iovec array of nr_args entries.  The buffers associated with the
+     * iovecs will be locked in memory and charged against the user's RLIMIT_MEMLOCK resource limit.
+     * See getrlimit(2) for more  informa‐ tion.   Additionally,  there  is a size limit of 1GiB per
+     * buffer.  Currently, the buffers must be anonymous, non-file-backed memory, such as that
+     * returned by malloc(3) or mmap(2) with the MAP_ANONYMOUS flag set.  It is expected that this
+     * limitation will be lifted in the future. Huge pages are supported as well. Note that the
+     * entire huge page will be pinned in the kernel, even if only a portion of it is used.
+     *
+     * After a successful call, the supplied buffers are mapped into the kernel and eligible for
+     * I/O.  To make use of them, the application must specify the IORING_OP_READ_FIXED or
+     * IORING_OP_WRITE_FIXED opcodes in the submis‐ sion  queue  entry (see the struct io_uring_sqe
+     * definition in io_uring_enter(2)), and set the buf_index field to the desired buffer index.
+     * The memory range described by the submission queue entry's addr and len fields must fall
+     * within the indexed buffer.
+     *
+     * It is perfectly valid to setup a large buffer and then only use part of it for an I/O, as
+     * long as the range is within the originally mapped region.
+     *
+     * An application can increase or decrease the size or number of registered buffers by first
+     * unregistering the existing buffers, and then issuing a new call to io_uring_register() with
+     * the new buffers.
+     *
+     * An application need not unregister buffers explicitly before shutting down the io_uring
+     * instance.
+     */
     REGISTER_BUFFERS        = 0,    /// `IORING_REGISTER_BUFFERS`
+
+    /**
+     * This operation takes no argument, and arg must be passed as NULL.  All previously registered
+     * buffers associated with the io_uring instance will be released.
+     */
     UNREGISTER_BUFFERS      = 1,    /// `IORING_UNREGISTER_BUFFERS`
+
+    /**
+     * Register files for I/O.  arg contains a pointer to an array of nr_args file descriptors
+     * (signed 32 bit integers).
+     *
+     * To make use of the registered files, the IOSQE_FIXED_FILE flag must be set in the flags
+     * member of the struct io_uring_sqe, and the fd member is set to the index of the file in the
+     * file descriptor array.
+     *
+     * Files are automatically unregistered when the io_uring instance is torn down. An application
+     * need only unregister if it wishes to register a new set of fds.
+     */
     REGISTER_FILES          = 2,    /// `IORING_REGISTER_FILES`
+
+    /**
+     * This operation requires no argument, and arg must be passed as NULL.  All previously
+     * registered files associated with the io_uring instance will be unregistered.
+     */
     UNREGISTER_FILES        = 3,    /// `IORING_UNREGISTER_FILES`
+
+    ///
     REGISTER_EVENTFD        = 4,    /// `IORING_REGISTER_EVENTFD`
+
+    ///
     UNREGISTER_EVENTFD      = 5,    /// `IORING_UNREGISTER_EVENTFD`
+
+    ///
     REGISTER_FILES_UPDATE   = 6,    /// `IORING_REGISTER_FILES_UPDATE`
 }
 
@@ -453,21 +523,109 @@ private
     }
 }
 
-/// See: io_uring_setup(2)
+/**
+ * Setup a context for performing asynchronous I/O.
+ *
+ * The `io_uring_setup()` system call sets up a submission queue (SQ) and completion queue (CQ) with
+ * at least entries entries, and returns a file descriptor which can be used to perform subsequent
+ * operations on the io_uring instance. The submission and completion queues are shared between
+ * userspace and the kernel, which eliminates the need to copy data when initiating and completing
+ * I/O.
+ *
+ * See_Also: `io_uring_setup(2)`
+ *
+ * Params:
+ *   entries = Defines how many entries can submission queue hold.
+ *   p = `SetupParameters`
+ *
+ * Returns:
+ *     `io_uring_setup(2)` returns a new file descriptor on success. The application may then provide
+ *     the file descriptor in a subsequent `mmap(2)` call to map the submission and completion queues,
+ *     or to the `io_uring_register(2)` or `io_uring_enter(2)` system calls.
+ *
+ *     On error, -1 is returned and `errno` is set appropriately.
+ */
 int io_uring_setup(uint entries, scope ref SetupParameters p) @trusted
 {
     pragma(inline);
     return syscall(SYS_io_uring_setup, entries, &p);
 }
 
-/// See: io_uring_enter(2)
-int io_uring_enter(int fd, uint to_submit, uint min_complete, EnterFlags flags, const sigset_t* sig) @trusted
+/**
+ * Initiate and/or complete asynchronous I/O
+ *
+ * `io_uring_enter()` is used to initiate and complete I/O using the shared submission and
+ * completion queues setup by a call to `io_uring_setup(2)`. A single call can both submit new I/O
+ * and wait for completions of I/O initiated by this call or previous calls to `io_uring_enter()``.
+ *
+ * When the system call returns that a certain amount of SQEs have been consumed and submitted, it's
+ * safe to reuse SQE entries in the ring. This is true even if the actual IO submission had to be
+ * punted to async context, which means that the SQE may in fact not have been submitted yet. If the
+ * kernel requires later use of a particular SQE entry, it will have made a private copy of it.
+ *
+ * Note: For interrupt driven I/O (where `IORING_SETUP_IOPOLL` was not specified in the call to
+ *     `io_uring_setup(2)`), an application may check the completion queue for event completions without
+ *     entering the kernel at all.
+ *
+ * See_Also: `io_uring_enter(2)`
+ *
+ * Params:
+ *   fd = the file descriptor returned by io_uring_setup(2).
+ *   to_submit = specifies the number of I/Os to submit from the submission queue.
+ *   min_complete = If the `IORING_ENTER_GETEVENTS` bit is set in flags, then the system call will attempt
+ *        to wait for `min_complete` event completions before returning. If the io_uring instance was configured
+ *        for polling, by specifying IORING_SETUP_IOPOLL in the call to io_uring_setup(2), then
+ *        min_complete has a slightly different meaning.  Passing a value of 0 instructs the kernel to
+ *        return any events which are already complete, without blocking. If min_complete is a non-zero
+ *        value, the kernel will still return immediately if  any completion  events are available.  If
+ *        no event completions are available, then the call will poll either until one or more
+ *        completions become available, or until the process has exceeded its scheduler time slice.
+ *   flags = Behavior modification flags - `EnterFlags`
+ *   sig = a pointer to a signal mask (see `sigprocmask(2)`); if sig is not `null`, `io_uring_enter()`
+ *         first replaces the current signal mask by the one pointed to by sig, then waits for events to
+ *         become available in the completion queue, and then restores the original signal mask. The
+ *         following `io_uring_enter()` call:
+ *
+ *         ```
+ *         ret = io_uring_enter(fd, 0, 1, IORING_ENTER_GETEVENTS, &sig);
+ *         ```
+ *
+ *         is equivalent to atomically executing the following calls:
+ *
+ *         ```
+ *         pthread_sigmask(SIG_SETMASK, &sig, &orig);
+ *         ret = io_uring_enter(fd, 0, 1, IORING_ENTER_GETEVENTS, NULL);
+ *         pthread_sigmask(SIG_SETMASK, &orig, NULL);
+ *         ```
+ *
+ *         See the description of `pselect(2)` for an explanation of why the sig parameter is necessary.
+ *
+ * Returns:
+ */
+int io_uring_enter(int fd, uint to_submit, uint min_complete, EnterFlags flags, const sigset_t* sig = null) @trusted
 {
     pragma(inline);
-    return syscall(SYS_io_uring_enter, fd, to_submit, min_complete, flags, null, sigset_t.sizeof);
+    return syscall(SYS_io_uring_enter, fd, to_submit, min_complete, flags, sig, sigset_t.sizeof);
 }
 
-/// See: io_uring_register(2)
+/**
+ * Register files or user buffers for asynchronous I/O.
+ *
+ * The `io_uring_register()` system call registers user buffers or files for use in an `io_uring(7)`
+ * instance referenced by fd.  Registering files or user buffers allows the kernel to take long term
+ * references to internal data structures or create long term mappings of application memory,
+ * greatly reducing per-I/O overhead.
+ *
+ * See_Also: `io_uring_register(2)
+ *
+ * Params:
+ *   fd = the file descriptor returned by a call to io_uring_setup(2)
+ *   opcode = code of operation to execute on args
+ *   arg = Args used by specified operation. See `RegisterOpCode` for usage details.
+ *   nr_args = number of provided arguments
+ *
+ * Returns: On success, io_uring_register() returns 0.  On error, -1 is returned, and errno is set accordingly.
+ */
 int io_uring_register(int fd, RegisterOpCode opcode, const(void)* arg, uint nr_args)
 {
     pragma(inline);
