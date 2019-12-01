e9ad4792c4ca391b2ea5e15ca74e0ff7d48570bc
Tomáš Chaloupka <chalucha@gmail.com>
2019-12-01 13:33:03 +0100

    ci tweaks But travis doesn't use required kernels anyway..

--- betterc_ut.sh
+++ betterc_ut.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -e
+set -ex
 
 # As dub sometimes sucks..
 ${DC} -of=during-test-unittest-bc -debug -g -unittest -w -betterC -vcolumns -Isource/ source/during/*.d source/during/tests/*.d
--- source/during/tests/package.d
+++ source/during/tests/package.d
@@ -4,10 +4,21 @@ version (unittest)
 {
     version (D_BetterC)
     {
+        import core.stdc.stdio;
         extern(C) void main()
         {
-            import core.stdc.stdio;
-            printf("All unit tests have been run successfully.\n");
+            runTests!("API tests", during.tests.api);
+            printf("\nAll unit tests have been run successfully.\n");
+        }
+
+        void runTests(string desc, alias symbol)()
+        {
+            printf(">> " ~ desc ~ ":\n");
+            static foreach(u; __traits(getUnitTests, symbol))
+            {
+                printf("> testing " ~ __traits(getAttributes, u)[0] ~ "\n");
+                u();
+            }
         }
     }
 }
