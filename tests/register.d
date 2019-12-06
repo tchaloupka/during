b31dba34b958135305bec6f02170c6e3979951bf
Tomáš Chaloupka <chalucha@gmail.com>
2019-12-06 11:06:54 +0100

    reg buffers test

--- tests/register.d
+++ tests/register.d
@@ -3,11 +3,59 @@ module during.tests.register;
 import during;
 import during.tests.base;
 
+import core.stdc.stdlib;
+
 @("buffers")
 unittest
 {
-    version (D_BetterC) errmsg = "Not implemented";
-    else throw new Exception("Not implemented");
+    // prepare uring
+    Uring io;
+    auto res = io.setup(4);
+    assert(res >= 0, "Error initializing IO");
+
+    void* ptr;
+
+    // single array
+    {
+        ptr = malloc(4096);
+        assert(ptr);
+        scope (exit) free(ptr);
+
+        ubyte[] buffer = (cast(ubyte*)ptr)[0..4096];
+        auto r = io.registerBuffers(buffer);
+        assert(r == 0);
+        r = io.unregisterBuffers();
+        assert(r == 0);
+    }
+
+    // multidimensional array
+    {
+        alias BA = ubyte[];
+        ubyte[][] mbuffer;
+        ptr = malloc(4 * BA.sizeof);
+        assert(ptr);
+        scope (exit)
+        {
+            foreach (i; 0..4)
+            {
+                if (mbuffer[i] !is null) free(&mbuffer[i][0]);
+            }
+            free(&mbuffer[0]);
+        }
+
+        mbuffer = (cast(BA*)ptr)[0..4];
+        foreach (i; 0..4)
+        {
+            ptr = malloc(4096);
+            assert(ptr);
+            mbuffer[i] = (cast(ubyte*)ptr)[0..4096];
+        }
+
+        auto r = io.registerBuffers(mbuffer);
+        assert(r == 0);
+        r = io.unregisterBuffers();
+        assert(r == 0);
+    }
 }
 
 @("files")
